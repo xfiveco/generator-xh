c4d30ed0a0d7ae925e79dadba96e3a7d13a3a9d9
Urszula Ciaputa <urszula.ciaputa@xhtmlized.com>
2015-02-03 13:00:33 +0100

    Solve https://github.com/xhtmlized/generator-xh/issues/134#issuecomment-72625073

--- app/templates/grunt/usemin.js
+++ app/templates/grunt/usemin.js
@@ -5,6 +5,29 @@
 module.exports = function(grunt) {
   'use strict';
 
+  /**
+   * Ensure there are no duplicates in generated concat config.
+   *
+   * This is needed because concat configuration is always extended,
+   * not replaced, in useminPrepare task, which results in multiplication
+   * when running watch.
+   */
+  function simplifyGeneratedConfig(context) {
+    var files = context.options.generated.files;
+    var uniqueFiles = [];
+
+    uniqueFiles = files.reduceRight(function (result, fileObj) {
+      if (!result.some(function (fo) { return fo.dest === fileObj.dest; })) {
+        result.push(fileObj);
+      }
+
+      return result;
+    }, []).reverse();
+
+    context.options.generated.files = uniqueFiles;
+    return context.options.generated;
+  }
+
   grunt.config('useminPrepare', {
     html: {
       src: '<%%= xh.build %>',
@@ -22,10 +45,25 @@ module.exports = function(grunt) {
       dest: '<%%= xh.dist %>',
       root: '<%%= xh.src %>',
       flow: {
-        steps: {'js': ['concat'], 'css': ['concat'] },
-        post: {},
+        steps: {
+          js: ['concat'],
+          css: ['concat']
+        },
+        post: {
+          js: [{
+            name: 'concat',
+            createConfig: simplifyGeneratedConfig
+          }],
+          css: [{
+            name: 'concat',
+            createConfig: simplifyGeneratedConfig
+          }]
+        },
         htmlmin: {
-          steps: {'js': ['concat', 'uglifyjs'], 'css': ['concat'] },
+          steps: {
+            js: ['concat', 'uglifyjs'],
+            css: ['concat']
+          },
           post: {}
         }
       }
